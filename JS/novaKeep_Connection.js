1a204a9ffbfd73e8047082ac6c66e245073447b3
310lbenson@gmail.com <310lbenson@gmail.com>
2020-10-07 20:14:14 -0400

    Added new webpages to project. One to add a new character, one to view the table and a homepage.

---
+++ JS/novaKeep_Connection.js
@@ -0,0 +1,12 @@
+﻿var mysql = require('SQLEXPRESS');
+
+var con = mysql.createConnection({
+    host: "SQLEXPRESS",
+    user: "ACT-Student",
+    password: ""
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
